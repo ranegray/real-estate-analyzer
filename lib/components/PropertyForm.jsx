e490df725006fc74a5f53e9b478ffb80a7b7e4fc
Rane Gray <ranegray830@gmail.com>
2023-04-16 15:43:05 +0200

    Defined data struture and start form

--- lib/components/PropertyForm.jsx
+++ lib/components/PropertyForm.jsx
@@ -2,10 +2,30 @@ const { useState } = require("react")
 
 const PropertyForm = () => {
     const [formData, setFormData] = useState({
+        // property details
         propertyName: '',
         address: '',
-        purchasePrice: 0,
-
+        purchasePrice: null,
+        closingCost: null,
+        // loan details
+        loanDownPayment: null,
+        loanLength: null,
+        loanRate: null,
+        // income
+        rentalIncome: null,
+        // expenses TODO make these default percentages
+        propertyTaxes: null,
+        insurance: null,
+        maintenance: null,
+        vacancy: null,
+        capEx: null,
+        managementFees: null,
+        water: null,
+        gas: null,
+        electric: null,
+        HOA: null,
+        garbage: null,
+        otherEx: null
     })
 
     const handleChange = () => {
@@ -18,7 +38,10 @@ const PropertyForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            
+            <label htmlFor="">property nickname</label>
+            <input type="text" />
+            <label htmlFor="">address</label>
+            <input type="text" />
         </form>
     )
 }
