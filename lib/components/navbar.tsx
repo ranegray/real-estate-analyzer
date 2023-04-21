70f38a43f7ae591d0275762786842e63a8918752
Rane Gray <ranegray830@gmail.com>
2023-04-21 21:48:02 +0200

    Styled menu icon

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
-import { Bars3Icon } from '@heroicons/react/24/outline' 
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function NavBar() {
   return (
-    <header className="flex justify-between items-center w-full p-3 border-b-2 border-neutral-700 bg-neutral-900">
-      <h1 className="text-3xl font-semibold flex items-center"><Image src={"/dark_logo.png"} alt="" width={24} height={24} className="mr-1.5 hover:animate-spin-slow" />hauscel</h1>
+    <header className="flex w-full items-center justify-between border-b-2 border-neutral-700 bg-neutral-900 p-3">
+      <h1 className="flex items-center text-3xl font-semibold">
+        <Image
+          src={"/dark_logo.png"}
+          alt=""
+          width={24}
+          height={24}
+          className="mr-1.5 hover:animate-spin-slow"
+        />
+        hauscel
+      </h1>
       <nav>
-        <Bars3Icon className="h-8 w-8 stroke-2 text-neutral-600"/>
+        <Bars3Icon className="h-8 w-8 stroke-2 text-neutral-700" />
       </nav>
     </header>
   );
