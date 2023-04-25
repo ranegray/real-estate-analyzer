ba2e8b669188e026f02ea03925ad3108ba25145c
Rane Gray <ranegray830@gmail.com>
2023-04-25 11:25:17 +0200

    added helper function for determining monthly mortgage payment

--- lib/utilities/calculations.ts
+++ lib/utilities/calculations.ts
@@ -1,20 +1,38 @@
-export const calculateDownPayment = (purchasePrice, downPayment) => {
-    // downPayment will be saved as float (i.e., 0.05, 0.2)
+const calculateDownPayment = (purchasePrice: number, downPayment: number) => {
     return purchasePrice * downPayment
 }
 
-export const calculateTotalLoanPayment = (purchasePrice, downPayment) => {
-    // pass in amount of downPayment from calculateDownPayment
+const calculatePrincipal = (purchasePrice: number, downPayment: number) => {
+    const downPaymentAmount = calculateDownPayment(purchasePrice, downPayment)
+    return purchasePrice - downPaymentAmount
 }
 
-export const calculateMortgage = (principal, interestRate, termLength) => {
-    // pass in principal from calculateTotalLoanPayment
-    // interestRate divided by 12 to get monthly
-    // termLength multiplied by 12 to get months
+const calculateMonthlyInterest = (interestRate : number) => {
+    return interestRate / 12
 }
 
-export const sumExpenses = (expenses) => {
-    // iterate over object and sum expenses
+const calculateMonthsOnLoan = (termLength : number) => {
+    return termLength * 12
 }
 
-export const 
+export const calculateMonthlyMortgagePayment = (purchasePrice: number, downPayment: number, interestRate: number, termLength: number) => {
+    const P = calculatePrincipal(purchasePrice, downPayment)
+    const i = calculateMonthlyInterest(interestRate)
+    const n = calculateMonthsOnLoan(termLength)
+
+    const mortgagePayment = P * ((i * (1 + i) ** n) / ((1 + i) ** n - 1))
+
+    return Math.round(mortgagePayment * 100) / 100
+}
+
+calculateMonthlyMortgagePayment(100000, 0.2, 0.05, 10)
+
+// export const calculateExpenses = (mortgagePayment: number, expenses: number[]) => {
+//     // iterate over object and sum expenses
+//     // has to take calcualteMortgage as well
+// }
+
+// export const calculateCashFlow = (rentalIncome: number, mortgagePayment: number, expenses: number[]) => {
+//     const totalExpenses = calculateExpenses(expenses)
+//     return rentalIncome - totalExpenses
+// }
