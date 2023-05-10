cd32701081d4fee48b95987a2716a05bdd7f69b9
Rane Gray <ranegray830@gmail.com>
2023-05-10 13:04:05 +0200

    added styling and formatting to property list

--- lib/components/propertylist/propertylist.tsx
+++ lib/components/propertylist/propertylist.tsx
@@ -1,16 +1,28 @@
+import Link from "next/link";
+
 export default function PropertyList({ properties = [] }) {
   const isEmpty = properties.length < 1;
-  console.log(properties);
+  const formatCurrency = new Intl.NumberFormat(undefined, {
+    style: "currency",
+    currency: "USD",
+  });
+
   return isEmpty ? (
     <p>You're not tracking any properties</p>
   ) : (
     properties.map((item) => {
       return (
-        <div key={item.id}>
-          <h2>{item.name}</h2>
-          <h3>{item.address}</h3>
-          <p>{item.purchasePrice}</p>
-          <p>{item.analysis}</p>
+        <div key={item.id} className="m-3 rounded-md border border-neutral-700">
+          <Link href={`/${item.id}`}>
+            <h2 className="text-lg font-semibold">{item.name}</h2>
+            <h3 className="font-semibold">{item.address}</h3>
+            <p>
+              {item.city}, {item.state}
+            </p>
+            <p>{formatCurrency.format(item.purchasePrice)}</p>
+            <p>Cash flow: {item.analysis}</p>
+            <p>CoC ROI: {item.analysis}</p>
+          </Link>
         </div>
       );
     })
