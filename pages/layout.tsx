582ffe98c6acd604c29b5524017e807845fe2259
Rane Gray <ranegray830@gmail.com>
2023-05-15 23:19:24 +0200

    cleaning code

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,6 +1,5 @@
 import NavBar from "@/lib/components/navbar";
 import Footer from "@/lib/components/footer";
-import MobileMenu from "@/lib/components/mobilemenu";
 
 export default function Layout({ children }) {
   return (
